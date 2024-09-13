16e0884b9373206b992c9de503fd6c1892ef6d6b
owenstuckman <ostuckman@outlook.com>
2024-09-13 17:10:02 -0400

    P1 for N2
    easy

---
+++ Week 1/MC1_P1_ostuckman.m
@@ -0,0 +1,25 @@
+% Program calculates drug elimination from the body%
+% Written by Owen Stuckman on September 13, 2024%
+
+clc
+clear
+
+% Receive user input for variables to input into formula%
+
+% C0 %
+initConc = input("Input the initial concentration in milligrams(mg): \n");
+% tsub1/2 %
+estHalfLife = input("\nInput the estimated half life in hours(hrs): \n");
+% t %
+timePassed = input("\nInput the time that has passed in hours(hrs): \n");
+
+% Calculate elimination rate and current concentration%
+
+% k %
+elimRate = log(2) / (estHalfLife);
+% C(t) %
+finalConc = initConc * exp((-1) * (elimRate) * (timePassed));
+
+disp(newline);
+disp("The current concentration of the drug in the body is: (for a given t in mg or hrs/mg)")
+disp(finalConc);
