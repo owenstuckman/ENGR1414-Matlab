09c601698550751468c849c7300d3d03df7997c4
owenstuckman <ostuckman@outlook.com>
2024-09-13 22:13:06 -0400

    done with hw 1

---
+++ Week 1/MC1_P2_ostuckman.m
@@ -0,0 +1,12 @@
+% Land Cover Calculator N3 P2 %
+% Made by Owen Stuckman on September 13, 2024 %
+
+forest = input("Input the area od the forest land cover: \n");
+
+lawn = input("\nInput the area of the lawn land cover: \n");
+
+paved = input("\nInput the area of the paved land cover: \n");
+
+totalArea = ((forest*0.2)+(lawn*.3)+(paved*.95))/(lawn+forest+paved);
+
+disp(totalArea);
---
+++ Week 1/MC1_P3_ostuckman.m
@@ -0,0 +1,15 @@
+% Problem 3: Solar Cell Calculator %
+% Made by Owen Stuckman : September 13, 2024 %
+
+% Get user input
+avgSolarRadiation = input("Average solar radiation in kWh/m2/day \n");
+
+amtSolarPanels = input("\nQuantity of solar panels to install: \n");
+
+% Calculate total energy produced
+totEnergyProd = (1.67 * amtSolarPanels) * (.23) * (avgSolarRadiation) * (.75);
+
+% Display total
+disp(newline);
+disp(totEnergyProd);
+
