9fd2b6d3ea812227a7680c253319ce7cd4cbc2d5
owenstuckman <ostuckman@outlook.com>
2024-10-01 12:21:40 -0400

    outline n5 complete

--- N5/N5.m
+++ N5/N5.m
@@ -1,1 +1,43 @@
+% Waste Divergence Calculator %
+% Adapted from
+% https://cleanriver.com/resource/how-to-measure-your-waste-diversion-rate/ %
+% Waste weight estimations:
+% https://www.lincoln.ne.gov/files/sharedassets/public/v/1/ltu/utilities/solid-waste-management/recycling/commercial-fact-sheets/estimating-material-weight.pdf%
+% 150 gal capacity big belly solar %
+% 35 gal typical outdoor can
+
+
+
+% rerun external loop 
+
+while True 
+
+% Converting weight of recycling / weight of total waste to: %
+
+
+% Written by Owen Stuckman on October 1, 2024 %
+
+disp("This program works to take the cans you are using and find the waste diversion rate.")
+% display table of common capacities %
+
+disp(newline);
+disp("Input the Capacity of the Cans you are using");
+disp(newline);
+
+
+% Get inputs req for calc %
+capacityTrashCan = input()
+
+% Calc Rate %
+
+% Output Comparison to Goal %
+
+% Display common strategies to improve %
+
+% loop for 3,6,9,12 month plans
+
+% display - give primary
+
+% rerun %
+
 
